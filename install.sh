6dc234968366457684d782b9249e2bcf3ebe55d5
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-05 23:26:26 +0200

    Made install.sh work from any directory

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install_neovim() {
 	curl -JLO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 	tar xzvf nvim-linux64.tar.gz
 	rm -rdf nvim-linux64.tar.gz
-	mv ~/nvim-linux64 ~/.local/bin/
+	mv nvim-linux64 ~/.local/bin/
 	ln -s ~/.local/bin/nvim-linux64/bin/nvim ~/.local/bin
 }
 
