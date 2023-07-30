61fbf2150f2a983f55d0b3642366c593c4a50758
geloman <geloman-likes-rust@users.noreply.github.com>
2023-07-30 12:21:01 +0800

    Make neovim install script posix compliant

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 install_neovim() {
-	[[ -z $(which nvim 2> /dev/null) ]] || return
-	cd $HOME && curl -s -JLO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+	[ -z "$(which nvim 2> /dev/null)" ] || return
+	cd "$HOME" && curl -s -JLO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 	tar xzvf nvim-linux64.tar.gz
 	rm -rdf nvim-linux64.tar.gz
-	mv $HOME/nvim-linux64 $HOME/.neovim
-	[ -d ~/.local ] || mkdir ~/.local && [ -d ~/.local/bin ] || mkdir ~/.local/bin
-	ln -s $HOME/.neovim/bin/nvim $HOME/.local/bin/
+	mv "$HOME"/nvim-linux64 "$HOME"/.neovim
+	[ -d ~/.local ] || mkdir ~/.local
+	[ -d ~/.local/bin ] || mkdir ~/.local/bin
+	ln -s "$HOME"/.neovim/bin/nvim "$HOME"/.local/bin/
 }
 
 install_neovim
